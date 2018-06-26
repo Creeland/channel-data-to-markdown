bcdae7ac8b83660ff49f5540b3fb9e7e8e40891a
Creeland A. Provinsal <cree@egghead.io>
2018-06-26 17:02:19 -0500

    add input and search for channel id

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 require('dotenv').config()
 const axios = require('axios')
-const {map} = require('lodash')
+const {map, head} = require('lodash')
 const fs = require('fs-extra')
 const json2md = require('json2md')
 const moment = require('moment')
+var Input = require('prompt-input');
 
 const file = './output.md'
 
-const getVideos = () => {
+const getChannel = (channel) => {
   try {
-    return axios.get(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=UUz-BYvuntVRt_VpfR6FKXJw&key=${process.env.GOOGLE_API_KEY}`)
+    return axios.get(`https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=${channel}&key=${process.env.GOOGLE_API_KEY}`)
   } catch (error) {
     console.error(error)
   }
 }
 
-const countVideos = async () => {
-  const videos = getVideos()
-    .then(response => {
+const getChannelId = (channel) => {
+  getChannel(channel).then(response => {
+    const id = head(response.data.items).contentDetails.relatedPlaylists.uploads
+    getVideos(id).then(response => {
       formattedVideos = map(response.data.items, video => {
         const {title, description, publishedAt} = video.snippet
         return json2md([
@@ -29,10 +31,25 @@ const countVideos = async () => {
         ])
       })
       fs.outputFile(file, formattedVideos)
+    }).catch(error => {
+      console.error(error)
     })
-    .catch(error => {
-      console.log(error)
-    })
+  })
 }
 
-countVideos()
+const getVideos = (id) => {
+  try {
+    return axios.get(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=${id}&key=${process.env.GOOGLE_API_KEY}`)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const input = new Input({
+  name: 'channel',
+  message: "Enter the channel's name: "
+})
+
+input.ask(channel => {
+  getChannelId(channel)
+})
